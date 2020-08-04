43e1999267f7a1ba3f24c80ee0e6ccb7e2cd1e0c
Fumito Ito <fumito.ito@nex.nikkei.com>
2020-08-04 23:15:39 +0900

    initial commit

---
+++ SwiftyRemoteConfig/SwiftyRemoteConfig.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyRemoteConfig.h
+//  SwiftyRemoteConfig
+//
+//  Created by 伊藤史 on 2020/08/04.
+//  Copyright © 2020 Fumito Ito. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyRemoteConfig.
+FOUNDATION_EXPORT double SwiftyRemoteConfigVersionNumber;
+
+//! Project version string for SwiftyRemoteConfig.
+FOUNDATION_EXPORT const unsigned char SwiftyRemoteConfigVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyRemoteConfig/PublicHeader.h>
+
+
