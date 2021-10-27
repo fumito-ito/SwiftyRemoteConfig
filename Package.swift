ed8841902b51cd45daccb4e960c5f8a306b00086
Fumito Ito <fumito.ito@nex.nikkei.com>
2021-10-28 01:25:53 +0900

    add support for macOS

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "SwiftyRemoteConfig",
     platforms: [
         .iOS(.v11),
+        .macOS(.v10_12),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
