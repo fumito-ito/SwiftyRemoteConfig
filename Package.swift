de70604af0f533e86514ae8ed22fa378e6ef3f3f
Fumito Ito <fumito.ito@nex.nikkei.com>
2022-01-06 23:22:35 +0900

    simplify package infomations and add support OSs
    according to firebase-ios-sdk, add support watchOS and tvOS

--- Package.swift
+++ Package.swift
@@ -2,45 +2,43 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-import class Foundation.ProcessInfo
-
-let shouldTest = ProcessInfo.processInfo.environment["TEST"] == "1"
-
-func resolveDependencies() -> [Package.Dependency] {
-    let firebaseRemoteConfig: Package.Dependency = .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMajor(from: "8.0.0"))
-
-    return [
-        firebaseRemoteConfig,
-    ]
-}
-
-func resolveTargets() -> [Target] {
-    let baseTarget = Target.target(
-        name: "SwiftyRemoteConfig",
-        dependencies: [
-            .product(name: "FirebaseRemoteConfig", package: "Firebase")
-        ],
-        path: "Sources")
-
-    let testTarget = Target.testTarget(
-        name: "SwiftyRemoteConfigTests",
-        dependencies: ["SwiftyRemoteConfig"])
-
-    return shouldTest ? [baseTarget, testTarget] : [baseTarget]
-}
 
 let package = Package(
     name: "SwiftyRemoteConfig",
     platforms: [
         .iOS(.v11),
         .macOS(.v10_12),
+        .tvOS(.v12),
+        .watchOS(.v6),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftyRemoteConfig",
-            targets: ["SwiftyRemoteConfig"]),
+            targets: ["SwiftyRemoteConfig"]
+        ),
     ],
-    dependencies: resolveDependencies(),
-    targets: resolveTargets()
+    dependencies: [
+        .package(
+            name: "Firebase",
+            url: "https://github.com/firebase/firebase-ios-sdk.git",
+            .upToNextMajor(from: "8.0.0")
+        ),
+    ],
+    targets: [
+        .target(
+            name: "SwiftyRemoteConfig",
+            dependencies: [
+                .product(name: "FirebaseRemoteConfig", package: "Firebase")
+            ],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "SwiftyRemoteConfigTests",
+            dependencies: [
+                "SwiftyRemoteConfig"
+            ],
+            path: "Tests"
+        ),
+    ]
 )
