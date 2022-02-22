a7be13a40bab2b50a706c37330118e7198ba3abd
Fumito Ito <fumito.ito@nex.nikkei.com>
2022-02-22 21:50:54 +0900

    bump to 8.12.1

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         .package(
             name: "Firebase",
             url: "https://github.com/firebase/firebase-ios-sdk.git",
-            .upToNextMajor(from: "8.0.0")
+            .upToNextMajor(from: "8.12.1")
         ),
     ],
     targets: [
