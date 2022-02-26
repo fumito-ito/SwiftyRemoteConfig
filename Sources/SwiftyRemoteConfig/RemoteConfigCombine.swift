e3167146e776bc0acaa401ae9f343d62d28dd3ff
Fumito Ito <fumito.ito@nex.nikkei.com>
2022-02-26 16:57:15 +0900

    update combine extension

---
+++ Sources/SwiftyRemoteConfig/RemoteConfigCombine.swift
@@ -0,0 +1,113 @@
+//
+//  RemoteConfigCombine.swift
+//  
+//
+//  Created by Fumito Ito on 2022/02/26.
+//
+
+import Foundation
+import Combine
+import FirebaseRemoteConfig
+
+@available(iOS 13.0, *)
+public extension RemoteConfig {
+    var combine: RemoteConfigCombine {
+        return RemoteConfigCombine(self)
+    }
+}
+
+@available(iOS 13.0, *)
+public class RemoteConfigCombine {
+    private let remoteConfig: RemoteConfig
+
+    init(_ remoteConfig: RemoteConfig) {
+        self.remoteConfig = remoteConfig
+    }
+}
+
+@available(iOS 13.0, *)
+extension RemoteConfigCombine {
+    class Subscription<S: Subscriber> {
+        private(set) var subscriber: S?
+        private(set) var cancellable: AnyCancellable?
+        let remoteConfig: RemoteConfig
+
+        init(subscriber: S, remoteConfig: RemoteConfig) {
+            self.subscriber = subscriber
+            self.remoteConfig = remoteConfig
+
+            cancellable = remoteConfig.combine
+                .fetchedPublisher()
+                .sink(
+                    receiveCompletion: { _ in },
+                    receiveValue: { [weak self] in self?.received() }
+                )
+        }
+
+        func cancelSubscription() {
+            cancellable?.cancel()
+            subscriber = nil
+        }
+
+        func received() {
+        }
+    }
+
+    final class RemoteConfigValueSubscription<S: Subscriber, T: RemoteConfigSerializable>: Subscription<S>, Combine.Subscription where S.Input == T.T, S.Failure == Never, T.T == T {
+        private let key: RemoteConfigKey<T>
+
+        init(subscriber: S, remoteConfig: RemoteConfig, key: RemoteConfigKey<T>) {
+            self.key = key
+            super.init(subscriber: subscriber, remoteConfig: remoteConfig)
+        }
+
+        public func request(_ demand: Subscribers.Demand) {}
+
+        public func cancel() {
+            super.cancelSubscription()
+        }
+
+        override func received() {
+            _ = subscriber?.receive(remoteConfig[key])
+        }
+    }
+
+    struct RemoteConfigValuePublisher<T: RemoteConfigSerializable>: Combine.Publisher where T.T == T {
+        public typealias Output = T.T
+        public typealias Failure = Never
+
+        private let remoteConfig: RemoteConfig
+        private let key: RemoteConfigKey<T>
+
+        init(remoteConfig: RemoteConfig, key: RemoteConfigKey<T>) {
+            self.remoteConfig = remoteConfig
+            self.key = key
+        }
+
+        public func receive<S>(subscriber: S) where S : Subscriber, Self.Failure == S.Failure, Self.Output == S.Input {
+            subscriber.receive(subscription: RemoteConfigValueSubscription(
+                subscriber: subscriber,
+                remoteConfig: remoteConfig,
+                key: key
+            ))
+        }
+    }
+}
+
+@available(iOS 13.0, *)
+public extension RemoteConfigCombine {
+    func fetchedPublisher() -> AnyPublisher<Void, Never> {
+        return remoteConfig.publisher(for: \.lastFetchTime)
+            .map({ _ in Void() })
+            .eraseToAnyPublisher()
+    }
+
+    func fetchedPublisher<T: RemoteConfigSerializable>(for keyPath: KeyPath<RemoteConfigKeys, RemoteConfigKey<T>>) -> AnyPublisher<T.T, Never> where T.T == T {
+        return RemoteConfigValuePublisher(remoteConfig: remoteConfig, key: RemoteConfigs.keyStore[keyPath: keyPath]).eraseToAnyPublisher()
+    }
+
+    func fetchedPublisher<KeyStore, T: RemoteConfigSerializable>(for keyPath: KeyPath<KeyStore, RemoteConfigKey<T>>,
+                                                                 adapter: RemoteConfigAdapter<KeyStore>) -> AnyPublisher<T.T, Never> where T.T == T {
+        return RemoteConfigValuePublisher(remoteConfig: remoteConfig, key: adapter.keyStore[keyPath: keyPath]).eraseToAnyPublisher()
+    }
+}
